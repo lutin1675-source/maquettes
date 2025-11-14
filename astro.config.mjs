f56765762bb60eafdfe6627ab57f3f6eacd29376
gpouleri <guillaume@ftprod.fr>
2025-11-15 00:55:33 +0100

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.SITE_URL ?? 'https://toton-maquette.com/',
 	base: '/',
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
