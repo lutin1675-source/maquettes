228f7ac3d16be0e6feeacaf6bb1784929e432c5b
gpouleri <guillaume@ftprod.fr>
2025-11-15 00:23:35 +0100

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL ?? 'https://maquette.com/',
+	site: process.env.SITE_URL ?? 'https://toton-maquette.com/',
 	base: '/',
 });
